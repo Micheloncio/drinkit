2d6837564d76c5e114724323fe57de6c75148ccf
Gemma Garcias <gemma.garcias.e@gmail.com>
2017-10-18 11:36:13 +0100

    v0.4

--- src/components/AllDrinks.js
+++ src/components/AllDrinks.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import {Grid, Row, Col, Thumbnail, Button} from 'react-bootstrap'
 import cocktailsApi from '../services/cocktailsApi'
 
 class AllDrinks extends Component {
@@ -9,7 +10,7 @@ class AllDrinks extends Component {
 		}
 	}
 
-	componentDidMount = () => { 
+	listAlcDrinks = () => { 
 		cocktailsApi.getAlcDrinks()
 		.then(res => {
 				this.setState({
@@ -18,21 +19,60 @@ class AllDrinks extends Component {
 			console.log(this.state)			
 		})
 	}
+	listNonAlcDrinks = () => {
+    cocktailsApi.getNonAlcDrinks()
+        .then(res => {
+                this.setState({
+                cocktailState: res.map(obj => obj.strDrink)
+            })
+            console.log(this.state)            
+        })
+    }
 
-
-
-
-
-
+    listOptionalDrinks = () => {
+    cocktailsApi.getOptionalDrinks()
+        .then(res => {
+                this.setState({
+                cocktailState: res.map(obj => obj.strDrink)
+            })
+            console.log(this.state)            
+        })
+    }
 
 	render() {
 		return (
-			<div>
-				<button>my button</button>
-				<ul>
-				{this.state.cocktailState.map((name,i) => <li key={i}>{name}</li>)}
-				</ul>
-			</div>)
+			 <Grid>
+			    <Row>
+			    <Col xs={6} md={4}>
+			      <Thumbnail>
+			      	<Button onClick={this.listAlcDrinks} bsStyle="primary" width={242} height={200}>Button</Button>&nbsp;
+			        <h3>Thumbnail label</h3>
+			        <ul>
+						{this.state.cocktailState.map((name,i) => <li key={i}>{name}</li>)}
+					</ul>
+			      </Thumbnail>
+			    </Col>
+			    <Col xs={6} md={4}>
+			      <Thumbnail>
+			        <Button onClick={this.listNonAlcDrinks} bsStyle="primary">Button</Button>&nbsp;
+			        <h3>Thumbnail label</h3>
+			        <ul>
+						{this.state.cocktailState.map((name,i) => <li key={i}>{name}</li>)}
+					</ul>
+			      </Thumbnail>
+			    </Col>
+			    <Col xs={6} md={4}>
+			      <Thumbnail>
+			        <Button onClick={this.listOptionalDrinks} bsStyle="primary">Button</Button>&nbsp;
+			        <h3>Thumbnail label</h3>
+			        <ul>
+						{this.state.cocktailState.map((name,i) => <li key={i}>{name}</li>)}
+					</ul>
+			      </Thumbnail>
+			    </Col>
+			    </Row>
+			  </Grid>
+			)
 		
 	}
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,6 @@ import React from 'react'
 import './Footer.css'
 
 const Footer= ()=>(
-<footer className="page-footer indigo right-on-small-only pt-0">
       <footer className="page-footer indigo center-on-small-only pt-0">
         {/*Footer Links*/}
         <div className="container">
@@ -10,26 +9,21 @@ const Footer= ()=>(
           <div className="row">
             {/*First column*/}
             <div className="col-md-12">
-              <div className="footer-socials mb-5 flex-right">
-                {/*Facebook*/}
-                <a className="icons-sm fb-ic"><i className="fa fa-facebook fa-lg white-text mr-md-4"> </i></a>
-                {/*Twitter*/}
-                <a className="icons-sm tw-ic"><i className="fa fa-twitter fa-lg white-text mr-md-4"> </i></a>
-                {/*Instagram*/}
-                <a className="icons-sm ins-ic"><i className="fa fa-instagram fa-lg white-text mr-md-4"> </i></a>
-                {/*Pinterest*/}
-                <a className="icons-sm pin-ic"><i className="fa fa-pinterest fa-lg white-text"> </i></a>
-                <div className="container-fluid">
-                   © 2017 Copyright: <a href="https://www.MDBootstrap.com"> MDBootstrap.com </a>
-                </div>
+              <div className="footer-socials flex-center">
+                <a className="icons-sm fb-ic" href='https://www.facebook.com'><i className="fa fa-facebook fa-lg white-text mr-md-4"> </i></a>
+                <a className="icons-sm tw-ic" href='https://twitter.com'><i className="fa fa-twitter fa-lg white-text mr-md-4"> </i></a>
+                <a className="icons-sm ins-ic" href='https://www.instagram.com'><i className="fa fa-instagram fa-lg white-text mr-md-4"> </i></a>
+                <a className="icons-sm pin-ic" href='https://www.pinterest.es'><i className="fa fa-pinterest fa-lg white-text"> </i></a>
+              </div>
+              <div className="container-fluid">
+                 © 2017 Copyright: <a href="https://www.MDBootstrap.com"> MDBootstrap.com </a>
+              </div>
             </div>
             {/*/First column*/}
           </div>
           {/*/First row*/}
         </div>
-        </div>
       </footer>
-    </footer>
 )
 
 export default Footer
