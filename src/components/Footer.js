b3c7e507e65cdae261217fe2194f0a1dc92f1127
Micheloncio <Michel@Seth-2.local>
2017-10-19 17:54:07 +0200

    :)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,26 +3,18 @@ import './Footer.css'
 
 const Footer= ()=>(
       <footer className="page-footer indigo center-on-small-only pt-0">
-        {/*Footer Links*/}
-        <div className="container">
-          {/*First row*/}
-          <div className="row">
-            {/*First column*/}
-            <div className="col-md-12">
-              <div className="footer-socials flex-center">
-                <a className="icons-sm fb-ic" href='https://www.facebook.com'><i className="fa fa-facebook fa-lg white-text mr-md-4"> </i></a>
-                <a className="icons-sm tw-ic" href='https://twitter.com'><i className="fa fa-twitter fa-lg white-text mr-md-4"> </i></a>
-                <a className="icons-sm ins-ic" href='https://www.instagram.com'><i className="fa fa-instagram fa-lg white-text mr-md-4"> </i></a>
-                <a className="icons-sm pin-ic" href='https://www.pinterest.es'><i className="fa fa-pinterest fa-lg white-text"> </i></a>
+          <div className="container">
+            <div className="row rowMargin">
+              <div className="col-md-5 col-md-offset-1">
+                   © 2017 Copyright  <a href="http://www.alcoholicos-anonimos.org/v_portal/apartados/apartado.asp" target="_blank"> Alcholicos Anonimos </a>
               </div>
-              <div className="container-fluid">
-                 © 2017 Copyright: <a href="https://www.MDBootstrap.com"> MDBootstrap.com </a>
+              <div className="col-md-4 col-md-offset-2">
+                   Created by  <a href="https://github.com/GemmaGarcias" target="_blank"> Gemma, </a>
+                   <a href="https://github.com/codeshakti" target="_blank"> Victor, </a>
+                   <a href="https://github.com/Micheloncio" target="_blank"> Michel </a>
               </div>
             </div>
-            {/*/First column*/}
           </div>
-          {/*/First row*/}
-        </div>
       </footer>
 )
 
