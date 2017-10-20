e3a5248df52197c900ac7e898fdb0e5ce566c4e1
Gemma Garcias <gemma.garcias.e@gmail.com>
2017-10-20 12:03:12 +0100

    css3

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,7 +18,7 @@ class Home extends Component {
 			<div className="container">
 			  <div className="jumbotron">
 			    <h1 className='home'>DRINK IT</h1>
-			    <h3>The definitive cocktails and drinks finder</h3>     
+			    <h3 className="homeh3">The definitive cocktails and drinks finder</h3>     
 			    <p></p>
 			  </div>    
 			</div>
